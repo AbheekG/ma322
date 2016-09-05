6d7500350e2d90289b37cf5755a64bc920d7ecc7
Abheek Ghosh <abheekghosh.ag@gmail.com>
2016-09-05 17:14:05 +0530

    partially complete lab 4

---
+++ lab4/q3/q3.m
@@ -0,0 +1,11 @@
+cd ..; nfd = @newtonForwardDifference; cd q3;
+
+x = 1:0.5:6;
+y = atan(x);
+
+value = 0:0.25:8;
+value = value';
+newtonForwardDifference_value = nfd(x, y, value);
+exact_value = atan(value);
+[newtonForwardDifference_value, exact_value, abs(newtonForwardDifference_value - exact_value)]
+plot(value, newtonForwardDifference_value, col='r', value, atan(value), col='b');
